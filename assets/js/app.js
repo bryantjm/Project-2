8446404477ae18c04d2cce5ba8f4b9e5f8fae416
bryantjm <bryantjm@iupui.edu>
2013-03-18 23:43:58 -0400

    first commit
    edited and revised version of project 1 with new nav buttons

---
+++ assets/js/app.js
@@ -0,0 +1,59 @@
+;(function($, window) {
+	
+	$('.toggle').click(function(e) {
+		var $t = $(this);
+		var id = $t.attr('href');
+		var target = $(id);
+		
+		if(target.hasClass('show')) {
+			target.removeClass('show');
+		}
+		else {
+			target.addClass('show');
+		}
+		
+		e.preventDefault();
+	});
+	
+	$('.bars').click(function(e) {
+		var $t = $(this);
+		
+		if($t.hasClass('rotate')) {
+			$t.removeClass('rotate');
+		}
+		else {
+			$t.addClass('rotate');
+		}
+				
+		e.preventDefault();
+	});
+	
+	$('.type-buttons a').click(function() {
+		var $buttons = $('.type-buttons a');
+		var $t       = $(this);
+		var length   = $buttons.length;
+		var type     = $t.attr('href').replace('#', '');
+		
+		$buttons.removeClass('primary');
+		$t.addClass('primary');
+		
+		for(x = 1; x <= length; x++) {
+			$('body').removeClass('type-'+x);
+		}
+		
+		$('body').addClass(type);
+		
+		$('.toggle').removeClass('rotate');
+		$('.show').removeClass('show');
+		
+		if(type) {
+			$('.nav').hide();
+			
+			setTimeout(function() {
+				$('.nav').show();
+				$('.nav').attr('style', '');
+			}, 500);
+		}
+	});
+	
+}(jQuery, this));
